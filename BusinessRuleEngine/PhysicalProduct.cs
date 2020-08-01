7793fb111be02254607a508140cfb21e5b6554fc
bishalroy <broy@stats.com>
2020-08-01 21:19:11 +0530

    Adding Physical Product

---
+++ BusinessRuleEngine/PhysicalProduct.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessRuleEngine
+{
+    class PhysicalProduct
+    {
+        public PhysicalProduct()
+        {
+            Console.WriteLine("Packing Slip Generated");
+            Console.WriteLine("Commision Payment to Agent Generated");
+        }
+    }
+}
