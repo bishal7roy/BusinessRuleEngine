50169e4124a28e1ca904846bcc23c7e64e7b70d5
bishalroy <broy@stats.com>
2020-08-01 21:30:52 +0530

    Adding TDD for Royalty Department, Royalty Department Class and using it from Book Class

--- BusinessRuleEngine/Book.cs
+++ BusinessRuleEngine/Book.cs
@@ -10,7 +10,8 @@ namespace BusinessRuleEngine
     {
         public Book() : base()
         {
-
+            RoyaltyDepartment royalty = new RoyaltyDepartment();
+            royalty.Royalty(true);    //RoyaltyDepartment will be called to provide duplicate slip
         }
     }
 }
--- BusinessRuleEngineTests/UnitTest1.cs
+++ BusinessRuleEngineTests/UnitTest1.cs
@@ -13,7 +13,22 @@ namespace BusinessRuleEngineTests.Test
             ProductDetails productDetails = new ProductDetails();
             var result = productDetails.InputUserValue(1);
             Assert.AreEqual(result, 1);
+        }
 
+        [TestMethod]
+        public void RoyaltyDepartment_ReturnsTrue()
+        {
+            RoyaltyDepartment royalty = new RoyaltyDepartment();
+            var result = royalty.Royalty(true);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void RoyaltyDepartment_ReturnsFalse()
+        {
+            RoyaltyDepartment royalty = new RoyaltyDepartment();
+            var result = royalty.Royalty(false);
+            Assert.IsFalse(result);
         }
     }
 }
